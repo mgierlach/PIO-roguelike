e56125c29378867b3698c1d422facd9701a5619d
Mateusz <mgierlach5@gmail.com>
2016-06-01 18:10:53 +0200

    uaktualniony Item, dodana metoda w ItemContainer i "zwyczajne" testy w ItemContainerTest

--- core/src/com/pio/roguelike/ItemContainer.java
+++ core/src/com/pio/roguelike/ItemContainer.java
@@ -1,54 +1,76 @@
-package com.pio.roguelike.ItemContainer;
+package com.pio.roguelike;
 
 import java.util.ArrayList;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class ItemContainer {
-    ArrayList itemList;
-    int quantity;
-    FreeTypeFontGenerator generator;
-    FreeTypeFontParameter parameter;
+    ArrayList<Item> itemList;
     BitmapFont font;
-    
-    itemContainer() {
+    final Color bg_color = new Color(0x343d46ff);
+    final Color text_color = new Color(0xeff1f5ff);
+    final Color text_color_sp = new Color(0xebcb8bff);
+
+    public ItemContainer() {
         itemList = new ArrayList();
     }
     
-    itemContainer(BitMapFont font) {
+    public ItemContainer(BitmapFont font) {
         itemList = new ArrayList();
         this.font = font;
     }
     
-    void addItem(Item item){
+    public void addItem(Item item){
         itemList.add(item);
     }
     
-    void removeItem(Item item){
-        itemList.remove(item);
+    public void containerInformation(){
+        for( int i=0; i < itemList.size(); i++ ) {
+            System.out.println("Nazwa przedmiotu: " + itemList.get(i).name );
+            System.out.println("Nazwa przedmiotu: " + itemList.get(i).price );
+            System.out.println("Nazwa przedmiotu: " + itemList.get(i).id + "\n" );
+        }
     }
     
-    void showItems(){
-        float width= 80f;
-        float height = 80f;
+    public void removeItem(Item item) { itemList.remove(item); }
+    
+    public void showItems(){
+        float width = 300;
+        float height = (font.getLineHeight() + 5) * itemList.size() + 20f;
         
-        float start_w = 20f;
-        float start_h = 20f;
-        Color text_normal = new Color(0xebcb8bff);
-        Color green = new Color(0xa3be8cff);
-        
+        float start_w = 250f;
+        float start_h = 300f - height / 2f;
+
+        ShapeRenderer shape = new ShapeRenderer();
         shape.begin(ShapeRenderer.ShapeType.Filled);
-        shape.setColor(green);
+        shape.setColor(bg_color);
         shape.rect(start_w, start_h, width, height);
         shape.end();
-        
-        for ( int i=0; i<itemList.size(); i++ ){
+
+        float yoffset = 10f;
+        for (Item item : itemList) {
             Batch batch = new SpriteBatch();
             batch.begin();
-            font.setColor(text_normal);
-            font.draw(batch, "Name: " + itemList.get(i).name, start_w + 1f, start_h + 1f);
-            font.draw(batch, "Price: " + itemList.get(i).price, start_w + 1f, start_h + 11f);
-            font.draw(batch, "ID: " + itemList.get(i).id, start_w + 1f, start_h + 21f);
+            String item_type = new String();
+            if (item instanceof Weapon) {
+                item_type = "W";
+            }
+            else if (item instanceof Armor) {
+                item_type = "A";
+            }
+            else {
+                item_type = "?";
+            }
+            font.setColor(text_color_sp);
+            font.draw(batch, item_type, start_w + 10f, start_h + height - yoffset);
+            font.setColor(text_color);
+            font.draw(batch, item.name, start_w + 40f, start_h + height - yoffset);
+            yoffset += 5f + font.getLineHeight();
             batch.end();
         }
     }
